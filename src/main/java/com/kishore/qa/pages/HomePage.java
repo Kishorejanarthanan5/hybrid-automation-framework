3f4b55a22191ee234cd71adc2a4b256936109419
Kishorejanarthanan5 <jkmay98@gmail.com>
2026-04-17 17:32:44 +0530

    refactor(pages): integrate waitutils in homepage and loginpage

--- src/main/java/com/kishore/qa/pages/HomePage.java
+++ src/main/java/com/kishore/qa/pages/HomePage.java
@@ -1,23 +1,31 @@
 package com.kishore.qa.pages;
 
+import com.kishore.qa.utils.WaitUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HomePage {
 
     private final WebDriver driver;
+    private final WaitUtils wait;
     private final By signupLoginLink = By.cssSelector("a[href='/login']");
     private final By logoutLink = By.cssSelector("a[href='/logout']");
+    private final By productsLink = By.cssSelector("a[href='/products']");
 
     public HomePage(WebDriver driver){
         this.driver = driver;
+        this.wait = new WaitUtils(driver);
     }
 
     public void clickSignupLogin(){
-        driver.findElement(signupLoginLink).click();
+        wait.waitForElementClickable(signupLoginLink).click();
     }
 
     public boolean isLogoutLinkDisplayed() {
-        return driver.findElement(logoutLink).isDisplayed();
+        return wait.waitForElementVisible(logoutLink).isDisplayed();
+    }
+
+    public void clickProducts(){
+        wait.waitForElementClickable(productsLink).click();
     }
 }
--- src/main/java/com/kishore/qa/pages/LoginPage.java
+++ src/main/java/com/kishore/qa/pages/LoginPage.java
@@ -1,28 +1,31 @@
 package com.kishore.qa.pages;
 
+import com.kishore.qa.utils.WaitUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class LoginPage {
 
     private final WebDriver driver;
+    private final WaitUtils wait;
     private final By emailText = By.cssSelector("input[data-qa='login-email']");
     private final By passwordText = By.cssSelector("input[data-qa='login-password']");
     private final By loginBtn = By.cssSelector("button[data-qa='login-button']");
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WaitUtils(driver);
     }
     public void enterEmail(String email) {
-        driver.findElement(emailText).sendKeys(email);
+        wait.waitForElementVisible(emailText).sendKeys(email);
     }
 
     public void enterPassword(String password){
-        driver.findElement(passwordText).sendKeys(password);
+        wait.waitForElementVisible(passwordText).sendKeys(password);
     }
 
     public void clickLogin(){
-        driver.findElement(loginBtn).click();
+        wait.waitForElementClickable(loginBtn).click();
     }
 
     public void loginAs(String email, String password) {
