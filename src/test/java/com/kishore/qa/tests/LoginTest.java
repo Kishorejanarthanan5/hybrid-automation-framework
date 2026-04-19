c076cc0f297d6a5716e8375f3ff61da2219d2377
Kishorejanarthanan5 <jkmay98@gmail.com>
2026-04-19 20:35:08 +0530

    refactor(login): use homepage for valid login assert loginpage for invalid

--- src/test/java/com/kishore/qa/tests/LoginTest.java
+++ src/test/java/com/kishore/qa/tests/LoginTest.java
@@ -1,28 +1,31 @@
 package com.kishore.qa.tests;
 
 import com.kishore.qa.base.BaseTest;
-import com.kishore.qa.driver.DriverFactory;
 import com.kishore.qa.pages.HomePage;
 import com.kishore.qa.pages.LoginPage;
 import com.kishore.qa.utils.ConfigReader;
-import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTest extends BaseTest {
 
-
     @Test
-    public void testValidLogin(){
-        WebDriver driver = DriverFactory.getDriver();
+    public void testValidLogin() {
         HomePage homePage = new HomePage(driver);
         LoginPage loginPage = new LoginPage(driver);
-
         homePage.clickSignupLogin();
-        loginPage.enterEmail(ConfigReader.getProperty("valid.email"));
-        loginPage.enterPassword(ConfigReader.getProperty("valid.password"));
-        loginPage.clickLogin();
-
-        Assert.assertTrue(homePage.isLogoutLinkDisplayed(), "Login failed");
+        loginPage.loginAs(ConfigReader.getProperty("valid.email"), ConfigReader.getProperty("valid.password"));
+        // Strong assertion (correct page + correct validation)
+        Assert.assertTrue(homePage.isUserLoggedIn("kishore"),
+                "Login failed: Expected username not displayed after login");
+    }
+    @Test
+    public void testInvalidLogin() {
+        HomePage homePage = new HomePage(driver);
+        LoginPage loginPage = new LoginPage(driver);
+        homePage.clickSignupLogin();
+        loginPage.loginAs(ConfigReader.getProperty("invalid.email"), ConfigReader.getProperty("invalid.password"));
+        // Assertion for Invalid
+        Assert.assertTrue(loginPage.isLoginErrorDisplayed(),"Expected error message is not displayed");
     }
 }
