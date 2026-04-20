28e8d90073f70e9c082ded019e57424c5d7836a3
Kishorejanarthanan5 <jkmay98@gmail.com>
2026-04-20 09:27:20 +0530

    test(cart): add product add-to-cart test

--- src/test/java/com/kishore/qa/tests/ProductTest.java
+++ src/test/java/com/kishore/qa/tests/ProductTest.java
@@ -2,6 +2,7 @@ package com.kishore.qa.tests;
 
 import com.kishore.qa.base.BaseTest;
 import com.kishore.qa.driver.DriverFactory;
+import com.kishore.qa.pages.CartPage;
 import com.kishore.qa.pages.HomePage;
 import com.kishore.qa.pages.ProductsPage;
 import org.openqa.selenium.WebDriver;
@@ -12,13 +13,28 @@ public class ProductTest extends BaseTest {
 
     @Test
     public void testProductSearch() {
-        WebDriver driver = DriverFactory.getDriver();
         HomePage homePage = new HomePage(driver);
         ProductsPage productsPage = new ProductsPage(driver);
 
         homePage.clickProducts();
         productsPage.searchProduct("Dress");
 
-        Assert.assertTrue(productsPage.isSearchResultsDisplayed(),"Search results are not displayed properly");
+        Assert.assertTrue(productsPage.isSearchResultsDisplayed(),
+                "Search results are not displayed properly");
+    }
+
+    @Test
+    public void testAddToCart() {
+        HomePage homePage = new HomePage(driver);
+        ProductsPage productsPage = new ProductsPage(driver);
+        CartPage cartPage = new CartPage(driver);
+
+        homePage.clickProducts();
+
+        productsPage.addFirstProductToCart();
+        productsPage.clickViewCartOnModal();
+
+        Assert.assertTrue(cartPage.isCartPageDisplayed(),
+                "Cart page not displayed after adding product");
     }
 }
